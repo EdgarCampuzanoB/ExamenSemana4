bf40a129ef07386a64605b67674d24e19c4f4f17
EdgarCampuzanoB <passwordgithub.1226>
2021-11-19 09:48:04 -0600

    Inicio Examen

---
+++ src/main/java/com/example/ExamenSemana4/ExamenSemana4Application.java
@@ -0,0 +1,13 @@
+package com.example.ExamenSemana4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSemana4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenSemana4Application.class, args);
+	}
+
+}
